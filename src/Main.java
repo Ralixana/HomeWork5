de887cb93cfc4b0b4932f6f413a4655e4b920518
Olesya <lesya.garsiya@gmail.com>
2023-05-17 01:37:53 +0500

    осталось 3 задание

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         System.out.println("Task 1");
 
-        int clientOS = 0;
+        int clientOS = 1;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
@@ -12,7 +12,7 @@ public class Main {
 
         System.out.println("Task 2");
 
-        int clientDeviceYear = 2015;
+        int clientDeviceYear = 2013;
         if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientOS == 1 && clientDeviceYear >= 2015) {
@@ -27,6 +27,7 @@ public class Main {
         System.out.println("Task 3");
 
         int year = 2021;
+        if (year % 4 =)
 
         System.out.println("Task 4");
 
@@ -39,6 +40,34 @@ public class Main {
             System.out.println("Потребуется дней: 3");
         } else {
             System.out.println("Доставки нет");
+        }
+
+        System.out.println("Task 5");
+
+        int monthNumber = 13;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
         }
     }
 }
