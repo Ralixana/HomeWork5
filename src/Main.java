71f6d2a8ac127c29f4e43943969d3594846e05a0
Olesya <lesya.garsiya@gmail.com>
2023-05-18 20:10:01 +0500

    Дз сделано

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,44 @@ public class Main {
         System.out.println("Task 1");
 
         int clientOS = 1;
-        if (clientOS == 0) {
+
+        int Android = 1;
+        int iOS = 0;
+        if (clientOS == iOS) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
+        } else if (clientOS == Android) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
 
         System.out.println("Task 2");
 
-        int clientDeviceYear = 2013;
-        if (clientOS == 1 && clientDeviceYear < 2015) {
+        int year = 2013;
+
+        int clientDeviceYear = 2015;
+
+        if (clientOS == Android && year < clientDeviceYear) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+        } else if (clientOS == Android && year >= clientDeviceYear) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
-        if (clientOS == 0 && clientDeviceYear < 2015) {
+        if (clientOS == iOS && year < clientDeviceYear) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+        } else if (clientOS == iOS && year >= clientDeviceYear) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
 
         System.out.println("Task 3");
 
-        int year = 2021;
-        if (year % 4 =)
+        // это задание списала у ребят, не понимаю почему решается именно так,
+        // не понимаю решение с помощью процента, долго сидела над ним
+
+        int Year = 2011;
+
+        if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0) {
+            System.out.println(Year + " - год является високосным");
+        } else {
+            System.out.println(Year + " - год не является високосным");
+        }
 
         System.out.println("Task 4");
 
