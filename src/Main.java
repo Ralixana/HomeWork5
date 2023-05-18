741b9098ef48c1f028d9a54613cbb2b4db7a933c
Olesya <lesya.garsiya@gmail.com>
2023-05-18 23:59:27 +0500

    Исправила названия переменных,перерешала 2 задание, поняла как решать 3, дополнила 4

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,80 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+    }
 
+    public static void task1() {
         System.out.println("Task 1");
 
         int clientOS = 1;
-        int Android = 1;
+        int android = 1;
         int iOS = 0;
         if (clientOS == iOS) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == Android) {
+        } else if (clientOS == android) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+    }
 
+    public static void task2() {
         System.out.println("Task 2");
 
-        int year = 2013;
+        int clientOS = 0;
+        int year = 2010;
+
+        int android = 1;
+        int iOS = 0;
         int clientDeviceYear = 2015;
 
-        if (clientOS == Android && year < clientDeviceYear) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientOS == Android && year >= clientDeviceYear) {
-            System.out.println("Установите версию приложения для Android по ссылке");
+        if (clientOS == android) {
+            if (year < clientDeviceYear) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        } else if (clientOS == iOS) {
+            if (year < clientDeviceYear) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
         }
-        if (clientOS == iOS && year < clientDeviceYear) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS == iOS && year >= clientDeviceYear) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        }
+    }
 
+    public static void task3() {
         System.out.println("Task 3");
 
-        // это задание списала у ребят, не понимаю почему решается именно так,
-        // не понимаю решение с помощью процента, долго сидела над ним
+        int year = 2000;
 
-        int Year = 2011;
-
-        if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0) {
-            System.out.println(Year + " - год является високосным");
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " - год является високосным");
         } else {
-            System.out.println(Year + " - год не является високосным");
+            System.out.println(year + " - год не является високосным");
         }
+    }
 
+    public static void task4 () {
         System.out.println("Task 4");
 
-        int deliveryDistance = 100;
+        int deliveryDistance = 50;
+        int deliveryTime = 1;
+
         if (deliveryDistance < 20) {
-            System.out.println("Потребуется дней: 1");
+            System.out.println("Потребуется дней: " + deliveryTime);
         } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
-            System.out.println("Потребуется дней: 2");
+            System.out.println("Потребуется дней: " + (deliveryTime + 1));
         } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуется дней: 3");
+            System.out.println("Потребуется дней: " + (deliveryTime + 2));
         } else {
             System.out.println("Доставки нет");
         }
+    }
 
+    public static void task5 () {
         System.out.println("Task 5");
 
         int monthNumber = 13;
